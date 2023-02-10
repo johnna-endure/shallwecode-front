167152590280b7d1c32b9c0f85eeab1b5a0e1779
choi woo seok <cwooseok60@gmail.com>
2023-02-10 22:17:49 +0900

    react router, redux 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom'
+import SignIn from './pages/SignIn'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/signin',
+    element: <SignIn />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    {/*<App />*/}
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { TextField } from '@mui/material'
+
+export default () => {
+  return (
+    <div>
+      <h1>로그인 페이지</h1>
+      <TextField></TextField>
+    </div>
+  )
+}
