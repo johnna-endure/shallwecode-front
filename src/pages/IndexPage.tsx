35ca12b576ad522946dadde48924512c59ca4b8f
choi woo seok <cwooseok60@gmail.com>
2023-02-26 11:50:16 +0900

    index 페이지에 로그인 링크 추가

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,9 +1,14 @@
+import { Link, useHref, useNavigate, useNavigation } from 'react-router-dom'
+
 export default (props: any) => {
+  // const goToPage = (url: string) => useHref(url)
+
   return (
     <div>
       인덱스 페이지
-      <h1>Drag me</h1>
-      {/*<h1 draggable>Drag me</h1>*/}
+      <div>
+        <Link to="/signin">로그인 페이지</Link>
+      </div>
     </div>
   )
 }
