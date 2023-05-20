6de155258368f7cec56e70ef325bfaf43243827f
choi woo seok <cwooseok60@gmail.com>
2023-05-20 17:38:08 +0900

    Modal 수정

--- src/molecules/Modal.tsx
+++ src/molecules/Modal.tsx
@@ -1,16 +1,38 @@
+import { ReactNode } from "react";
 import {
   CenterContentPanel,
   ComponentSize,
-  FullScreenBackground,
-} from '../atoms/Layout'
-import { ReactNode } from 'react'
+  FullScreenBackground
+} from "../atoms/Layout";
+import { CloseIconGray } from "../atoms/CloseIcon";
 
-export const Modal = (props: { size: ComponentSize; children?: ReactNode }) => {
+
+interface ModalProps {
+  open: boolean;
+  size: ComponentSize;
+  title?: string,
+  showCloseButton?: boolean;
+  children?: ReactNode;
+  onCancel?: () => {};
+}
+
+export const Modal = (props: ModalProps) => {
+  const defaultShowCloseButton = props?.showCloseButton ?? true;
   return (
-    <FullScreenBackground color={'bg-gray-400'}>
+    <FullScreenBackground color={"bg-gray-400"}>
       <CenterContentPanel size={props.size}>
-        {props?.children}
+        {/*닫기 버튼*/}
+        {defaultShowCloseButton &&
+          <div className={"flex justify-between"}>
+            <p className={"ml-2 text-xl font-bold"}>{props?.title}</p>
+            <CloseIconGray onClick={props?.onCancel} />
+          </div>
+        }
+        {/*모달 컨텐츠 영역*/}
+        <div className={"p-4 w-full h-5/6 overflow-y-scroll"}>
+          {props?.children}
+        </div>
       </CenterContentPanel>
     </FullScreenBackground>
-  )
-}
+  );
+};
