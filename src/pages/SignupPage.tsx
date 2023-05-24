9f9347f5e90da8b843ac8a89c06425b7d6ebfdf6
choi woo seok <cwooseok60@gmail.com>
2023-05-24 23:27:36 +0900

    주석 추가

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -4,6 +4,7 @@ import SignupForm from '../organisms/SignupForm'
 import { useParams } from 'react-router-dom'
 
 export default () => {
+  // TODO react router로 type 전달해서 받기, 값 없을 경우 에러 페이지로
   const { type } = useParams()
   return (
     <FullScreenBackground>
