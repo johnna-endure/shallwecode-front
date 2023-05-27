ce495dd50995109fd9520e1fa7ed0793cdad958d
choi woo seok <cwooseok60@gmail.com>
2023-05-28 01:23:39 +0900

    프리티어 포맷팅

--- src/organisms/SignupForm.tsx
+++ src/organisms/SignupForm.tsx
@@ -36,32 +36,51 @@ export default (props: SignupFormProps) => {
   }
   return (
     <>
-        <p className="mb-14 text-center text-3xl font-bold">회원가입</p>
-        <div className='w-[450px] m-auto'>
-        <p className='text-xs text-right'><span className='text-red-500'>*</span>는 필수 입력사항입니다</p>
+      <p className="mb-14 text-center text-3xl font-bold">회원가입</p>
+      <div className="w-[450px] m-auto">
+        <p className="text-xs text-right">
+          <span className="text-red-500">*</span>는 필수 입력사항입니다
+        </p>
 
-        <form className='flex flex-col items-center' onSubmit={handleSubmit}>
-          <div className='flex flex-col items-center gap-2'>
+        <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+          <div className="flex flex-col items-center gap-2">
             {props.type === 'password' && <IdPasswordFormField />}
             <FormField>
-              <TopLabelTextInput className="w-[340px] m-auto" labelText={'이메일'} name={'email'} required />
+              <TopLabelTextInput
+                className="w-[340px] m-auto"
+                labelText={'이메일'}
+                name={'email'}
+                required
+              />
             </FormField>
             <FormField>
-              <TopLabelTextInput className="w-[340px]" labelText={'이름'} name={'name'} required />
+              <TopLabelTextInput
+                className="w-[340px]"
+                labelText={'이름'}
+                name={'name'}
+                required
+              />
             </FormField>
             <FormField>
-              <TopLabelTextInput className="w-[340px]" labelText={'블로그'} name={'blogUrl'} />
+              <TopLabelTextInput
+                className="w-[340px]"
+                labelText={'블로그'}
+                name={'blogUrl'}
+              />
             </FormField>
             <FormField>
-              <TopLabelTextInput className="w-[340px]" labelText={'깃허브'} name={'githubUrl'} />
+              <TopLabelTextInput
+                className="w-[340px]"
+                labelText={'깃허브'}
+                name={'githubUrl'}
+              />
             </FormField>
           </div>
-            <button
-              className="rounded mt-10 p-3 w-[340px] text-white bg-[#00c473]">
-              가입하기
-            </button>
+          <button className="rounded mt-10 p-3 w-[340px] text-white bg-[#00c473]">
+            가입하기
+          </button>
         </form>
-        </div>
+      </div>
     </>
   )
 }
@@ -72,33 +91,38 @@ const IdPasswordFormField = () => {
   return (
     <>
       <FormField failureMessage={loginIdFailureMessage}>
-          <TopLabelTextInput
-            className="w-[340px]"
-            labelText={'아이디'}
-            name={'loginId'}
-            required
-            onChange={(e) => {
-              if (timeoutId) {
-                clearTimeout(timeoutId)
-              }
-              setTimeoutId(
-                setTimeout(async () => {
-                  const isValid = await authenticationApi.checkLoginId(
-                    e.target.value
-                  )
+        <TopLabelTextInput
+          className="w-[340px]"
+          labelText={'아이디'}
+          name={'loginId'}
+          required
+          onChange={(e) => {
+            if (timeoutId) {
+              clearTimeout(timeoutId)
+            }
+            setTimeoutId(
+              setTimeout(async () => {
+                const isValid = await authenticationApi.checkLoginId(
+                  e.target.value
+                )
 
-                  if (!isValid) {
-                    setLoginIdFailureMessage('이미 존재하는 아이디입니다')
-                  } else {
-                    setLoginIdFailureMessage('')
-                  }
-                }, 100)
-              )
-            }}
-          />
+                if (!isValid) {
+                  setLoginIdFailureMessage('이미 존재하는 아이디입니다')
+                } else {
+                  setLoginIdFailureMessage('')
+                }
+              }, 100)
+            )
+          }}
+        />
       </FormField>
       <FormField>
-        <TopLabelTextInput className="w-[340px]" labelText={'비밀번호'} name={'password'} required />
+        <TopLabelTextInput
+          className="w-[340px]"
+          labelText={'비밀번호'}
+          name={'password'}
+          required
+        />
       </FormField>
     </>
   )
